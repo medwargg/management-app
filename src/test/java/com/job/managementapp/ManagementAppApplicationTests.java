c01a7514bdc87c82a1ab61046b6dbe2dbba50ea7
Cesar Guerrero <medwargg@gmail.com>
2021-08-02 21:11:54 -0500

    Base project

---
+++ src/test/java/com/job/managementapp/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.managementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
